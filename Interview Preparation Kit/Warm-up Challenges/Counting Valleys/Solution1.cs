ca7b79b725629454c9147a3ce3202f6bfcc5d82f
Isuru Siriwardana <isurukdniss@gmail.com>
2019-08-08 10:40:30 +0530

    1st attempt

---
+++ Interview Preparation Kit/Warm-up Challenges/Counting Valleys/Solution1.cs
@@ -0,0 +1,54 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, string s) {
+        int valleyCount = 0;
+        int total = 0;
+
+        for(int i=0; i< n; i++){
+            if(total == 0 && s[i] == 'D'){
+                valleyCount++;
+                total--;
+            }
+            else {
+                if(s[i] == 'U')
+                {
+                    total++;
+                }
+                else {
+                    total--;
+                }
+            }
+        }
+        return valleyCount;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string s = Console.ReadLine();
+
+        int result = countingValleys(n, s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
